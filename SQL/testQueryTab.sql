4fc4d04c1a758d5440475780c70340deccbabe62
Nate Tolentino <ntole24@gmail.com>
2024-11-21 18:00:13 +0800

    Added to the testing file so that you can see the current tables in action

--- SQL/testQueryTab.sql
+++ SQL/testQueryTab.sql
@@ -1,7 +1,33 @@
-SELECT * FROM draftDB.items;
+SELECT
+	i.name,
+    si.unit_cost,
+    s.assigned_staff_id
+FROM 
+	items i
+JOIN
+	supplier_item_cost si on i.item_id = si.item_id
+JOIN
+	suppliers s ON si.supplier_id = s.supplier_id
+WHERE 
+	i.quantity > 100
+ORDER BY
+	i.item_id;
+    
+SELECT
+	i.name,
+    si.unit_cost,
+    c.name as company_name
+FROM 
+	items i
+JOIN
+	supplier_item_cost si on i.item_id = si.item_id
+JOIN
+	customers c ON i.item_id = c.item_id
+ORDER BY
+	i.item_id;
 
-SELECT * FROM draftDB.suppliers;
+SELECT * FROM items;
+SELECT * FROM suppliers;
+SELECT * FROM supplier_item_cost;
+SELECT * FROM customers;
 
-SELECT * FROM draftDB.customers;
-
-SELECT * FROM draftDB.staff;
