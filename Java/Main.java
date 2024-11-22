e6e7573aeaebe09cf8a65358e61415659c7c8adc
Nate Tolentino <ntole24@gmail.com>
2024-11-22 21:46:14 +0800

    Edited main a bit to allow for testing

--- Java/Main.java
+++ Java/Main.java
@@ -1,4 +1,3 @@
-import java.sql.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -7,13 +6,32 @@ public class Main {
         items i = new items();
         customers c = new customers();
         staff st = new staff();
+        purchases pu = new purchases();
+        sales sa = new sales();
+        jobs j = new jobs();
+        payrolls pa = new payrolls();
+        returns r = new returns();
+        // payment_details pm = new payment_details();
+        supplier_item_cost sic = new supplier_item_cost();
 
         /* .setValExceptSupID("ST104", "OnlyLabs Inc.", "+63-969-420-6869", "sales@OnlyLabs.com", "Muntinlupa City");
         s.addSupplier(); */
         /* c.setValExceptCusID("John Doe", "1234567890", "john.doe@example.com", "New York");
         System.out.println(c.addCustomer()); */
 
-        st.setValExceptStaID("Mark Santiago", Date.valueOf("2024-11-21"), null, "J104", "Employed");
-        st.addStaff();
+        /* st.setValExceptStaID("Mark Santiago", Date.valueOf("2024-11-21"), null, "J104", "Employed");
+        st.addStaff(); */
+        /* pu.setValExceptPurID("I100", Date.valueOf("2024-11-21"), 10, Date.valueOf("2026-01-01"));
+        pu.addPurchase(); */
+        /*sa.setValExceptPurID("C104", "I104", 10, Date.valueOf("2024-11-21"));
+        sa.addSale();*/
+        /* j.setValExceptJobID("Baker", 15000);
+        j.addJob(); */
+        /* pa.setValExceptJobID("ST104", "PM101");
+        pa.addPayroll(); */
+        /* r.setValExceptRetID("ST102", "SA106", "Damaged packaging", Date.valueOf("2024-11-22"));
+        r.addReturn(); */
+        sic.setVals("I100", "S101", 5);
+        sic.addSupplierItemCost();
     }
 }
